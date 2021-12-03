47c193571e89f5c7e4b223a29d33117e46981104
Paulinho <paulinho@agencia3w.com.br>
2021-12-03 17:03:52 -0300

    Login Page

---
+++ src/services/firebaseConnection.example.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXM7spf11KpZYMvqXuLWyEIJLApsoZ0SQ",
+  authDomain: "financas-f1a77.firebaseapp.com",
+  projectId: "financas-f1a77",
+  storageBucket: "financas-f1a77.appspot.com",
+  messagingSenderId: "976988445439",
+  appId: "1:976988445439:web:51c9e07866d0f382b80aca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
