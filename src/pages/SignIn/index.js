c1b8992bf5e2feaff4d907bbd27fd13d194e8b72
Paulinho <paulinho@agencia3w.com.br>
2021-12-09 16:51:51 -0300

    Loading Submit Form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { Platform } from 'react-native';
+import { Platform, ActivityIndicator, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 import { AuthContext } from '../../contexts/auth';
 
@@ -13,50 +13,59 @@ export default function SignIn() {
 
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-	const { signIn } = useContext(AuthContext);
+	const { signIn, loadingAuth } = useContext(AuthContext);
 
 	function handleLogin() {
 		signIn(email, password);
 	}
 
 	return (
-		<Background>
-			<Container
-				behavior={Platform.OS === 'ios' ? 'padding' : ''}
-				enabled
-			>
-				<Logo source={require('../../assets/Logo.png')} />
+		<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+			<Background>
+				<Container
+					behavior={Platform.OS === 'ios' ? 'padding' : ''}
+					enabled
+				>
+					<Logo source={require('../../assets/Logo.png')} />
 
-				<AreaInput>
-					<Input
-						placeholder="Email"
-						autoCorrect={false}
-						autoCapitalize="none"
-						value={email}
-						onChangeText={(text) => setEmail(text)}
-					/>
-				</AreaInput>
+					<AreaInput>
+						<Input
+							placeholder="Email"
+							autoCorrect={false}
+							autoCapitalize="none"
+							value={email}
+							onChangeText={(text) => setEmail(text)}
+						/>
+					</AreaInput>
 
-				<AreaInput>
-					<Input
-						placeholder="Senha"
-						autoCorrect={false}
-						autoCapitalize="none"
-						value={password}
-						secureTextEntry
-						onChangeText={(text) => setPassword(text)}
-					/>
-				</AreaInput>
+					<AreaInput>
+						<Input
+							placeholder="Senha"
+							autoCorrect={false}
+							autoCapitalize="none"
+							value={password}
+							secureTextEntry
+							onChangeText={(text) => setPassword(text)}
+						/>
+					</AreaInput>
 
-				<SubmitButton onPress={handleLogin}>
-					<SubmitText>Acessar</SubmitText>
-				</SubmitButton>
+					<SubmitButton onPress={handleLogin}>
+						{
+							loadingAuth ? (
+								<ActivityIndicator size={20} color="#FFF" />
+							) : (
+								<SubmitText>Acessar</SubmitText>
 
-				<Link onPress={() => navigation.navigate('SignUp')}>
-					<LinkText>Criar uma conta!</LinkText>
-				</Link>
+							)
+						}
+					</SubmitButton>
 
-			</Container>
-		</Background>
+					<Link onPress={() => navigation.navigate('SignUp')}>
+						<LinkText>Criar uma conta!</LinkText>
+					</Link>
+
+				</Container>
+			</Background>
+		</TouchableWithoutFeedback>
 	);
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,66 +1,76 @@
 import React, { useState, useContext } from 'react';
-import { Platform } from 'react-native';
+import { Platform, ActivityIndicator, Keyboard, TouchableWithoutFeedback } from 'react-native';
 
 import { AuthContext } from '../../contexts/auth';
 
 import {
-	Background, Container, Logo, AreaInput, Input, SubmitButton,
+	Background, Container, AreaInput, Input, SubmitButton,
 	SubmitText
 } from '../SignIn/styles';
 
 export default function SignUp() {
-	const [nome, setNome] = useState('Paulinho');
-	const [email, setEmail] = useState('paulinho@agencia3w.com.br');
-	const [password, setPassword] = useState('123456');
+	const [nome, setNome] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-	const { signUp } = useContext(AuthContext);
+	const { signUp, loadingAuth } = useContext(AuthContext);
 
 	function handleSignUp() {
 		signUp(email, password, nome);
 	}
 
 	return (
-		<Background>
-			<Container
-				behavior={Platform.OS === 'ios' ? 'padding' : ''}
-				enabled
-			>
+		<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+			<Background>
+				<Container
+					behavior={Platform.OS === 'ios' ? 'padding' : ''}
+					enabled
+				>
 
-				<AreaInput>
-					<Input
-						placeholder="Nome"
-						autoCorrect={false}
-						autoCapitalize="none"
-						value={nome}
-						onChangeText={(text) => setNome(text)}
-					/>
-				</AreaInput>
+					<AreaInput>
+						<Input
+							placeholder="Nome"
+							autoCorrect={false}
+							autoCapitalize="none"
+							value={nome}
+							onChangeText={(text) => setNome(text)}
+						/>
+					</AreaInput>
 
-				<AreaInput>
-					<Input
-						placeholder="Email"
-						autoCorrect={false}
-						autoCapitalize="none"
-						value={email}
-						onChangeText={(text) => setEmail(text)}
-					/>
-				</AreaInput>
+					<AreaInput>
+						<Input
+							placeholder="Email"
+							autoCorrect={false}
+							autoCapitalize="none"
+							value={email}
+							onChangeText={(text) => setEmail(text)}
+						/>
+					</AreaInput>
 
-				<AreaInput>
-					<Input
-						placeholder="Senha"
-						autoCorrect={false}
-						autoCapitalize="none"
-						value={password}
-						onChangeText={(text) => setPassword(text)}
-					/>
-				</AreaInput>
+					<AreaInput>
+						<Input
+							placeholder="Senha"
+							autoCorrect={false}
+							autoCapitalize="none"
+							value={password}
+							onChangeText={(text) => setPassword(text)}
+							secureTextEntry
+						/>
+					</AreaInput>
 
-				<SubmitButton onPress={handleSignUp}>
-					<SubmitText>Cadastrar</SubmitText>
-				</SubmitButton>
+					<SubmitButton onPress={handleSignUp}>
+						{
+							loadingAuth ? (
+								<ActivityIndicator size={20} color="#FFF" />
+							) : (
+								<SubmitText>Cadastrar</SubmitText>
 
-			</Container>
-		</Background>
+							)
+						}
+					</SubmitButton>
+
+				</Container>
+			</Background>
+		</TouchableWithoutFeedback>
 	);
 }
