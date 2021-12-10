fc11c793b34e1256ae36e172919d00133db136f4
Paulinho <paulinho@agencia3w.com.br>
2021-12-10 16:45:00 -0300

    Delete function

--- src/components/HistoricoList/index.js
+++ src/components/HistoricoList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import {
     Container,
@@ -8,16 +9,18 @@ import {
     Valor,
 } from './styles';
 
-export function HistoricoList({ data }) {
+export function HistoricoList({ data, deleteItem }) {
     return (
-        <Container>
-            <Tipo>
-                <Icon tipo={data.tipo}>
-                    <Feather name={data.tipo === 'despesa' ? 'arrow-down' : 'arrow-up'} size={20} color="#FFF" />
-                    <TipoText>{data.tipo}</TipoText>
-                </Icon>
-            </Tipo>
-            <Valor>R$ {data.valor}</Valor>
-        </Container>
+        <TouchableWithoutFeedback onLongPress={() => deleteItem(data)}>
+            <Container>
+                <Tipo>
+                    <Icon tipo={data.tipo}>
+                        <Feather name={data.tipo === 'despesa' ? 'arrow-down' : 'arrow-up'} size={20} color="#FFF" />
+                        <TipoText>{data.tipo}</TipoText>
+                    </Icon>
+                </Tipo>
+                <Valor>R$ {data.valor}</Valor>
+            </Container>
+        </TouchableWithoutFeedback>
     )
 }
